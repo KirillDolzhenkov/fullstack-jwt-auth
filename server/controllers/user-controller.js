d2f541f7031d2a28a19c2df779a6c2f2efb24f25
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2024-08-21 18:24:07 +0300

    generation jwt accsess & refresh tokens

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,16 @@
+const userService = require('../service/user-service')
+
 class UserController{
 
     async registration(req, res, next){
        try {
+           const {email, password} = req.body
+           const userData= await userService.registration(email, password)
+           res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
 
+           return res.json(userData)
        } catch (e) {
+           console.log(e)
        }
     }
     async login(req, res, next){
@@ -20,6 +27,7 @@ class UserController{
     }
     async activate(req, res, next) {
         try {
+
         } catch (e) {
         }
     }
---
+++ server/service/mail-service.js
@@ -0,0 +1,8 @@
+class MailService{
+
+    async sendActivationMail(email, link){
+        console.log(link)
+    }
+}
+
+module.exports = new MailService()
